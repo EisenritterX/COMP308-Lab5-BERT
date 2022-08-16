8b7b8b5ad31aa1d26473751c35c87b96991750bc
George Zhou <georgezhou89@hotmail.com>
2022-08-16 13:07:55 -0400

    Completed Bert Model

--- bert-ui/src/App.js
+++ bert-ui/src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+//1. Import Dependencies
+import * as tf from "@tensorflow/tfjs";
+import * as qna from "@tensorflow-models/qna";
+import Spinner from "react-bootstrap/Spinner";
+import { Fragment } from "react";
+
+const App = () => {
+  // 3. Setup references and state hooks
+  const passageRef = useRef(null);
+  const questionRef = useRef(null);
+  const [answer, setAnswer] = useState();
+  const [model, setModel] = useState(null);
+  const [showLoading, setShowLoading] = useState(true);
+
+  // 4. Load Tensorflow Model
+  const loadModel = async () => {
+    const loadedModel = await qna.load();
+    setModel(loadedModel);
+    setShowLoading(false);
+    console.log("Model loaded.");
+  };
+
+  // 5. Handle Questions
+  const answerQuestion = async (e) => {
+    if (e.which === 13 && model !== null) {
+      console.log("Question submitted.");
+      const passage = passageRef.current.value;
+      const question = questionRef.current.value;
+
+      const answers = await model.findAnswers(question, passage);
+      setAnswer(answers);
+      console.log(answers);
+    }
+  };
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  // 2. Setup input, question and result area
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {model == null ? (
+          <div>
+            <div>Model Loading</div>
+            {showLoading && (
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            )}
+          </div>
+        ) : (
+          <React.Fragment>
+            Passage
+            <textarea ref={passageRef} rows="30" cols="100"></textarea>
+            Ask a Question
+            <input
+              ref={questionRef}
+              onKeyPress={answerQuestion}
+              size="80"
+            ></input>
+            <br />
+            Answers
+            {answer
+              ? answer.map((ans, idx) => (
+                  <div>
+                    <b>Answer {idx + 1} - </b> {ans.text} (
+                    {Math.floor(ans.score * 100) / 100})
+                  </div>
+                ))
+              : ""}
+          </React.Fragment>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
